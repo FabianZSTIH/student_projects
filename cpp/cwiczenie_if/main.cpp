c96f5b36d2c62b8734a27ca10004c01046c7f59a
FabianZSTIH <fabian.kucz@e-zstih.edu.pl>
2024-01-18 13:53:30 +0000

    studentmachine send

---
+++ cpp/cwiczenie_if/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int x;
+cout<<"Podaj wiek"<<endl;
+cin>>x>>endl;
+if(x>18){
+cout<<"Jestes pewnoletni!"<<endl;
+}
+else{
+cout<<"Jestes niepewnoletni!"<<endl;
+}
+	return 0;
+}
