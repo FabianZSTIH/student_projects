2044f5c35eef453956b3961a6a90c13d8f88e1da
FabianZSTIH <fabian.kucz@e-zstih.edu.pl>
2024-04-04 12:47:28 +0000

    wyslanie pracy

--- cpp/spalanie/main.cpp
+++ cpp/spalanie/main.cpp
@@ -10,15 +10,18 @@ int main(){
     int tankowanie;
     int paliwo;
     float cena=5;
+    float cenaspalanie;
 
     cout<<"podaj odleglosc"<<endl;
     cin>>odleglosc;
+    cout<<"podaj liczbe osob"<<endl;
+    cin>>osoby;
     cout<<"podaj ilosc paliwa"<<endl;
     cin>>paliwo;
 
     spalanie=(srspalanie*odleglosc)/100;
-    tankowanie=(paliwo/40)+1
-
+    tankowanie=(paliwo/40)+1;
+    cenaspalanie=cena*srspalanie;
 
     cout<<"odleglosc "<<odleglosc<<"km"<<endl;
     cout<<"osoby "<<osoby<<endl;
